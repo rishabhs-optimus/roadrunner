96ba3a865d92d09e67df9b75b4e29e6c36bc5bde
vaishaliT-optimus <vaishali.taneja@optimusinfo.com>
2016-07-20 18:14:22 +0530

    reviews Pagination Functionality

--- app/pages/product-details/ui/pdp-reviews.js
+++ app/pages/product-details/ui/pdp-reviews.js
@@ -4,6 +4,7 @@ define([
 function($) {
 
 
+    var $reviewBellow = $('.c-reviews-bellow');
     var changeHeadingPosition = function() {
         var $heading, $container;
         $container = $('#reviewsTabCon').find('.pr-review-wrap');
@@ -82,12 +83,65 @@ function($) {
         var $sortBy = $('.pr-review-sort-box');
         $sortBy.find('label').text('Sort By:');
     };
+    var getCurentPage = function(paginationWrapper) {
+        var $paginationWrapper = paginationWrapper.find('.pr-pagination-bottom');
+        var $prev = $paginationWrapper.find('.pr-page-prev');
+        var $next = $paginationWrapper.find('.pr-page-next');
+        var currentPage = 1;
+        if ($prev.find('a').length > 0) {
+            var text = $prev.find('a').attr('onclick');
+            var pageNo = text.slice(text.indexOf('(') + 1, text.indexOf(','));
+            currentPage =  parseInt(pageNo) + 1;
+        }
+        return currentPage;
+    };
+    var createPaginationDropDown = function() {
+        if ($('.pr-page-nav').html() === null) {
+            return;
+        }
+        var totalReviewCount = $reviewBellow.find('.c-bellows__header').text();
+        totalReviewCount = totalReviewCount.slice(totalReviewCount.indexOf('(') + 1, totalReviewCount.indexOf(')'));
+
+        var $paginationWrapper = $('#reviewsTabCon');
+        var perPageCount = $paginationWrapper.find('.pr-pagination-top .pr-page-count strong').text();
+        perPageCount = perPageCount.split('-')[1];
+        var totalPages = Math.ceil(totalReviewCount / 30);
+        var options = [];
+        var $select = $('<select class="c-review-page-dropdown"></select>');
+        var currentPage = getCurentPage($paginationWrapper);
+        for (var i = 1; i <= totalPages; i++) {
+            var text = 'Page ' + i + ' of ' + totalPages;
+            if (currentPage === i) {
+                $select.append('<option value="' + i + '"selected="selected">' + text + '</option>');
+            } else {
+                $select.append('<option value=' + i + '>' + text + '</option>');
+            }
+        }
+        var $dropDownContainer = $('<div class="c-review-dropdown"></div>');
+        $dropDownContainer.append($select);
+        $('.pr-pagination-bottom').append($dropDownContainer);
+    };
+    var reviewPaginationDropDownChangeFunc = function() {
+        $('.c-review-page-dropdown').on('change', function() {
+            var value = $(this).val();
+            var $paginationWrapper = $('.pr-pagination-bottom');
+            var text = $paginationWrapper.find('.pr-page-nav a').attr('onclick');
+            var parts = text.split('getReviewsFromMeta(');
+            var secondpart = parts[1].split(/,(.+)?/)[1];
+            var newLink = parts[0] + 'getReviewsFromMeta(' + value + ',' + secondpart;
+            $('.c-temp-review-pagination-anchor').attr('onclick', newLink);
+            $('.c-temp-review-pagination-anchor').click();
+        });
+    };
 
     return {
         changeHeadingPosition: changeHeadingPosition,
         updatePaginationButtons: updatePaginationButtons,
         addNoRatingsSection: addNoRatingsSection,
         createRangeInReview: createRangeInReview,
-        transformSortBy: transformSortBy
+        transformSortBy: transformSortBy,
+        createPaginationDropDown: createPaginationDropDown,
+        getCurentPage: getCurentPage,
+        reviewPaginationDropDownChangeFunc: reviewPaginationDropDownChangeFunc
     };
 });
